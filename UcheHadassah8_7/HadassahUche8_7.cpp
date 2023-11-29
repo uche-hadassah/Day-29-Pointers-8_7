7ab000cc55ddaa70d6a4799862cb1e668ee9f73d
Hadassah Uche <uhadassah1@gmail.com>
2023-11-29 10:12:38 +0200

    Add project files.

---
+++ UcheHadassah8_7/HadassahUche8_7.cpp
@@ -0,0 +1,4 @@
+/*Write a void function that uses pointers to add a single character at the end of an existing 
+C-string. The string and the character should be taken as parameters. For example if str
+contains the C-string Hello World the function call addChar(str, ‘!’) should 
+change the string str to Hello World!*/
