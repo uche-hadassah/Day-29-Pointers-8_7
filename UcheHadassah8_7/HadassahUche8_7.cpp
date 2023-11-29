176b2975977043c883df12b03c5a12f6364cf722
Hadassah Uche <uhadassah1@gmail.com>
2023-11-29 10:28:36 +0200

    Yay~ I did it and without any help ^^

--- UcheHadassah8_7/HadassahUche8_7.cpp
+++ UcheHadassah8_7/HadassahUche8_7.cpp
@@ -2,3 +2,29 @@
 C-string. The string and the character should be taken as parameters. For example if str
 contains the C-string Hello World the function call addChar(str, ‘!’) should 
 change the string str to Hello World!*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+void addChar(char*, char);
+int main()
+{
+	char Str[100];
+	char x;
+	cout << "Enter a string:";
+	cin.getline(Str, 100);
+	cout << "Enter the charcter you wish to add at the end of the string:";
+	cin >> x;
+	addChar(Str, x);
+	cout << "The new string is:" << Str;
+}
+
+void addChar(char* str, char a)
+{
+	while (*str != '\0')
+	{
+		str++;
+	}
+	*str = a;
+	str++;
+	*str = '\0';
+}
